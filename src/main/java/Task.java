d37a9f8eca0e07e8822a9f745d080f742ef8130a
LucasNgui <e1525740@u.nus.edu>
2026-08-12 18:34:22 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private boolean isDone;
+    private final String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String cross = isDone ? "X" : " ";
+        return String.format("[%s] %s", cross, description);
+    }
+}
