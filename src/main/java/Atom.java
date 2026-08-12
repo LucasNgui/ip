f6ec29d64463e5f28c4c822f4018f8b8abbf9f52
LucasNgui <e1525740@u.nus.edu>
2026-08-12 16:34:36 +0800

    Atom.java: Update greeting

--- src/main/java/Atom.java
+++ src/main/java/Atom.java
@@ -1,5 +1,13 @@
 public class Atom {
     public static void main(String[] args) {
-
+        String name = "Atom";
+        String greeting = String.format("~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "Hi! I'm %s~☆ ヽ(*・ω・)ﾉ\n"
+                + "~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "Bye bye! (￣▽￣)ノ\n"
+                + "~~~~~~~~~~~~~~~~~~~~~~~~~\n",
+                name
+        );
+        System.out.println(greeting);
     }
 }
