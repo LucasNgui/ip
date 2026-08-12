7a6e59237ce5d223e19617653e9a49f443eedc8a
LucasNgui <e1525740@u.nus.edu>
2026-08-12 16:44:26 +0800

    Atom.java: Add greet and bye methods

--- src/main/java/Atom.java
+++ src/main/java/Atom.java
@@ -1,13 +1,23 @@
 public class Atom {
-    public static void main(String[] args) {
-        String name = "Atom";
+    private final static String name = "Atom";
+
+    private static void greet() {
         String greeting = String.format("~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 + "Hi! I'm %s~☆ ヽ(*・ω・)ﾉ\n"
-                + "~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                + "Bye bye! (￣▽￣)ノ\n"
                 + "~~~~~~~~~~~~~~~~~~~~~~~~~\n",
                 name
         );
         System.out.println(greeting);
+    }
+
+    private static void bye() {
+        String bye = "~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                + "Bye bye! (￣▽￣)ノ\n"
+                + "~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        System.out.println(bye);
+    }
+
+    public static void main(String[] args) {
+        greet();
     }
 }
