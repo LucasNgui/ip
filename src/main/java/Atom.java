5b111e880ca35a3f6b80e2c5d493a1899ab8952f
LucasNgui <e1525740@u.nus.edu>
2026-08-12 18:53:48 +0800

    Atom.java: Add marking and unmarking tasks

--- src/main/java/Atom.java
+++ src/main/java/Atom.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
-import java.util.Scanner;;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Atom {
     private final static String name = "Atom";
     private final Scanner scanner;
-    private final ArrayList<String> tasks;
+    private final ArrayList<Task> tasks;
 
     public Atom() {
         greet();
@@ -32,10 +34,53 @@ public class Atom {
     private void list() {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
         int i = 1;
-        for (String s : tasks) {
+        for (Task s : tasks) {
             System.out.println(i + ". " + s);
             i++;
         }
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    }
+
+    private boolean checkMarkTask(String s) {
+        // check if the string matches "mark %d" or "unmark %d"
+        int number;
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        if (!matcher.find()) {
+            return false;
+        }
+        number = Integer.parseInt(matcher.group());
+        if (number < 0 || number > tasks.size()) {
+            return false;
+        }
+
+        if (s.equals("mark " + number)) {
+            markTask(number);
+        }
+        else if (s.equals("unmark " + number)) {
+            unmarkTask(number);
+        } else {
+            return false;
+        }
+
+        readLine();
+        return true;
+    }
+
+    private void markTask(int number) {
+        tasks.get(number - 1).mark();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Awesome! Marking this task as done");
+        System.out.println(tasks.get(number - 1));
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    }
+
+    private void unmarkTask(int number) {
+        tasks.get(number - 1).unmark();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Ok. Marking this task as undone");
+        System.out.println(tasks.get(number - 1));
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     }
 
@@ -50,11 +95,14 @@ public class Atom {
                 readLine();
                 break;
             default:
+                if (checkMarkTask(line)) {
+                    break;
+                }
                 String echo = "~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                         + line
                         + "\n~~~~~~~~~~~~~~~~~~~~~~~~~\n";
                 System.out.println(echo);
-                tasks.add(line);
+                tasks.add(new Task(line));
                 readLine();
         }
     }
