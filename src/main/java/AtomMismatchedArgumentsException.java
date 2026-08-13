330d69c27022f121f6969ec98e723c02c44f7cf6
LucasNgui <e1525740@u.nus.edu>
2026-08-13 15:05:56 +0800

    Add AtomMismatchedArgumentsException.java

---
+++ src/main/java/AtomMismatchedArgumentsException.java
@@ -0,0 +1,6 @@
+public class AtomMismatchedArgumentsException  extends AtomException {
+    public AtomMismatchedArgumentsException(String command, int expected, int found) {
+        super(String.format("Oh no! %s expects %d arguments but got %d.",
+                command, expected, found));
+    }
+}
