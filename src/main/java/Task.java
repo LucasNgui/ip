ac41fe1e381dd8d2223c6f88d4608fecec8e3081
LucasNgui <e1525740@u.nus.edu>
2026-08-13 10:56:00 +0800

    Task.java: Update attributes to protected

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,6 @@
 public class Task {
-    private boolean isDone;
-    private final String description;
+    protected boolean isDone;
+    protected final String description;
 
     public Task(String description) {
         this.description = description;
