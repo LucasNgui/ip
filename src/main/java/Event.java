cefaa03d2741cdfa557462badb21cf02097df643
LucasNgui <e1525740@u.nus.edu>
2026-08-18 14:40:22 +0800

    Event.java: Update toString method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,8 +10,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        String cross = isDone ? "X" : " ";
-        return String.format("[E][%s] %s (from: %s to: %s)",
-                cross, description, startTime, endTime);
+        return String.format("[E]%s (from: %s to: %s)",
+                super.toString(), startTime, endTime);
     }
 }
