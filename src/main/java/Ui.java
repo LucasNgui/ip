795e4cec3c34a06c5b45228add2788dbb0b0235a
LucasNgui <e1525740@u.nus.edu>
2026-08-19 11:31:43 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,46 @@
+public class Ui {
+    public static final String NAME = "Atom";
+
+    public void printWrappedText(String text) {
+        String wrapper = "~~~~~~~~~~~~~~~~~~~~~~~~~";
+        System.out.println(wrapper);
+        System.out.println(text);
+        System.out.println(wrapper);
+    }
+
+    public void greet() {
+        printWrappedText(String.format("Hi! I'm %s~☆ ヽ(*・ω・)ﾉ", NAME));
+    }
+
+    public void bye() {
+        printWrappedText("Bye bye! (￣▽￣)ノ");
+    }
+    
+    public void list(String list) {
+        printWrappedText(list);
+    }
+
+    public void mark(Task task) {
+        printWrappedText("Awesome! Marking this task as done:\n" + task);
+    }
+    
+    public void unmark(Task task) {
+        printWrappedText("Ok. Marking this task as undone:\n" + task);
+    }
+
+    public void add(Task task, int size) {
+        printWrappedText("Alright! I've added this task:\n"
+                + task
+                + String.format("\nYou now have %d tasks in the list.", size));
+    }
+
+    public void remove(Task task, int size) {
+        printWrappedText("Alright! I've removed this task:\n"
+                + task
+                + String.format("\nYou now have %d tasks in the list.", size));
+    }
+
+    public void printError(AtomException e) {
+        printWrappedText(e.getMessage());
+    }
+}
