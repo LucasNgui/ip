39f67b3186c531400d6f028de0a147d320ca1387
LucasNgui <e1525740@u.nus.edu>
2026-09-01 10:04:09 +0800

    Fix coding standard violations in task tests

--- src/test/java/atom/task/EventTest.java
+++ src/test/java/atom/task/EventTest.java
@@ -1,21 +1,21 @@
 package atom.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
 
     @Test
     public void mark() {
-        Event markedEvent = new Event("meeting",  "2020-10-10",  "2020-10-11");
+        Event markedEvent = new Event("meeting", "2020-10-10", "2020-10-11");
         markedEvent.mark();
         assertEquals("[E][X] meeting (from: Oct 10 2020 to: Oct 11 2020)", markedEvent.toString());
     }
 
     @Test
     public void unmark() {
-        Event unmarkedEvent = new Event("meeting",  "2020-10-10",  "2020-10-11");
+        Event unmarkedEvent = new Event("meeting", "2020-10-10", "2020-10-11");
         unmarkedEvent.mark();
         unmarkedEvent.unmark();
         assertEquals("[E][ ] meeting (from: Oct 10 2020 to: Oct 11 2020)", unmarkedEvent.toString());
@@ -24,6 +24,6 @@ public class EventTest {
     @Test
     public void testStringConversion() {
         assertEquals("[E][ ] meeting (from: Oct 10 2020 to: Oct 11 2020)",
-                new Event("meeting",  "2020-10-10",  "2020-10-11").toString());
+                new Event("meeting", "2020-10-10", "2020-10-11").toString());
     }
 }
