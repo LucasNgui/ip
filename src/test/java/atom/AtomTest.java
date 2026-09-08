0b309b240d3a2cf099a78f4963775d6b0a6c0a7e
LucasNgui <e1525740@u.nus.edu>
2026-09-08 10:32:03 +0800

    Add AtomTest.java

---
+++ src/test/java/atom/AtomTest.java
@@ -0,0 +1,17 @@
+package atom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AtomTest {
+    @Test
+    void greeting_isReturned() {
+        assertEquals("Hi, I'm Atom! ('^')/", new Atom().getGreeting());
+    }
+
+    @Test
+    void invalidInput_returnsUserFriendlyError() {
+        assertEquals("Oh no! I don't know what that means.", new Atom().getResponse("unknown"));
+    }
+}
